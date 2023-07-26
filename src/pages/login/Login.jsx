9d9ec031b03671b733872e9126feaad6b97ec584
zeru0429 <mihiretutigistu@gmail.com>
2023-07-26 18:29:59 +0300

    form validation inserted in to sign in page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,39 +1,102 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
 
 const Login = () => {
+  const [form, setForm] = useState({});
+  const [errors, setError] = useState({});
+
+  const setField = (field, value) => {
+    setForm({
+      ...form,
+      [field]: value
+    });
+
+    if (!!errors[field]) {
+      setError({
+        ...errors,
+        [field]: null
+      });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      console.log(form);
+    }
+  };
+
+  const validateForm = () => {
+    let isValid = true;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+  
+    // Validate email
+    if (!form.email || !emailRegex.test(form.email)) {
+      setError({
+        ...errors,
+        email: 'Please enter a valid email address'
+      });
+      isValid = false;
+    }
+  
+    // Validate password
+    if (!form.pass || !passRegex.test(form.pass)) {
+      setError({
+        ...errors,
+        pass: 'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character'
+      });
+      isValid = false;
+    }
+  
+    return isValid;
+  };
+
   return (
-    <div class="container center">
-    <main class="form-signin col-12 col-md-8 col-lg-6 col-xl-4">
-      <form>
-        <div class="text-center">
-          <img class="mb-4" src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" />
-          <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-        </div>
+    <Container className="d-flex mt-3 justify-content-center align-items-center vh-50">
+      <main className="form-signin">
+        <Form onSubmit={handleSubmit}>
+          <div className="text-center mb-4">
+            <img className="mb-4" src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" />
+            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+          </div>
 
-        <div class="form-floating mb-4">
-          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
-          <label for="floatingInput">Email address</label>
-        </div>
-        <div class="form-floating mb-4">
-          <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
-          <label for="floatingPassword">Password</label>
-        </div>
+          <Form.Group className="mb-4" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email"
+              required
+              value={form.email || ''}
+              onChange={(e) => setField('email', e.target.value)}
+              isInvalid={!!errors.email}
+            />
+            <Form.Control.Feedback type='invalid'>
+              {errors.email}
+            </Form.Control.Feedback>
+          </Form.Group>
 
-        <div class="checkbox mb-3">
-          <label>
-            <input type="checkbox" value="remember-me" /> Remember me
-          </label>
-        </div>
-        <div>
-          <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-        </div>
-        
-        <p class="mt-5 mb-3 text-muted text-center">&copy; 2017–2021</p>
-      </form>
-    </main>
+          <Form.Group className="mb-4" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" required placeholder="Password"
+              value={form.pass || ''}
+              onChange={(e) => setField('pass', e.target.value)}
+              isInvalid={!!errors.pass}
+            />
+            <Form.Control.Feedback type='invalid'>
+              {errors.pass}
+            </Form.Control.Feedback>
+          </Form.Group>
 
-  </div>
-  )
-}
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Remember me" />
+          </Form.Group>
 
-export default Login
+          <Button variant="primary" type="submit" className="w-100 mb-4">
+            Sign in
+          </Button>
+        </Form>
+      </main>
+    </Container>
+  );
+};
+
+export default Login;
