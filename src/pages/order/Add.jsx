2f3b9ed85a15c6c4926b653c20a11a451e33afc5
zeru0429 <mihiretutigistu@gmail.com>
2023-08-12 16:07:43 +0300

    fix merge probleam

--- src/pages/order/Add.jsx
+++ src/pages/order/Add.jsx
@@ -3,13 +3,13 @@ import axios from 'axios';
 import { Form, Button } from 'react-bootstrap';
 import AlertExample from '../../components/other/Alert';
 import { server, imageserver } from '../../constants';
-
 const Add = (props) => {
-  const { selectedProduct, selectedKind, totalPrice, handleProductChange, handleKindChange } = props;
+  const { selectedProduct, selectedKind,amount,remain_price,paid_price, totalPrice,state_of_order,phone, handleProductChange, handleKindChange,full_name } = props;
   const [form, setForm] = useState({});
   const [errors, setErrors] = useState({});
-
+  let cout = 3;
   useEffect(() => {
+    // Set default values for input fields
     const defaultValues = {};
     props.columns.forEach((column) => {
       defaultValues[column.field] = column.defaultValue || '';
@@ -17,32 +17,19 @@ const Add = (props) => {
     setForm(defaultValues);
   }, [props.columns]);
 
-  useEffect(() => {
-    if (selectedProduct) {
-      const filteredKinds = props.columns[1].options.filter(option => option.id === selectedProduct.id);
-      setKindOptions(filteredKinds);
-    }
-  }, [props.columns, selectedProduct]);
-
-  const calculateTotalPrice = () => {
-    if (selectedProduct && selectedKind) {
-      const priceColumn = selectedKind === 'home_made' ? 'home_price' : 'out_price';
-      const selectedProductData = props.columns[0].options.find(option => option.id === selectedProduct.id);
-      return amount * selectedProductData[priceColumn];
-    }
-    return 0;
-  };
-
-  useEffect(() => {
-    const calculatedTotalPrice = calculateTotalPrice();
-    setTotalPrice(calculatedTotalPrice);
-    setRemainPrice(calculatedTotalPrice - paid_price);
-  }, [amount, selectedProduct, selectedKind, paid_price]);
-
+ 
   const setField = (field, value) => {
-    setForm(prevForm => ({
+    setForm((prevForm) => ({
       ...prevForm,
       [field]: value,
+    }));
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setForm((prevForm) => ({
+      ...prevForm,
+      profile: file,
     }));
   };
 
@@ -57,77 +44,176 @@ const Add = (props) => {
         .post(`${imageserver}addNew${props.name}/`, formData)
         .then((response) => {
           props.setOpen(false);
-          // Show success message or handle response as needed
+          <AlertExample message={`Successfully Added`} variant='success' />;
           console.log(response.data);
         })
         .catch((error) => {
           console.log(error);
         });
     }
+    console.log(errors);
   };
 
   const validateForm = () => {
-    // Implement your form validation logic
-    return true; // Placeholder, replace with your validation
+    let isValid = true;
+    // Add your validation logic here
+    return isValid;
   };
+
+ 
 
   return (
     <>
-      <div className="add">
-        <div className="modal1">
-          {/* ... (your existing JSX) */}
-          <Form.Group key="product_name">
-            <Form.Label>product_name</Form.Label>
-            <Form.Control
-              required
-              as="select"
-              name="product_name"
-              value={selectedProduct ? selectedProduct.id : ''}
-              onChange={(e) => handleProductChange(Number(e.target.value))}
-              disabled={false} // You can adjust the disabled attribute as needed
-            >
-              <option value="">None</option>
-              {props.columns[0].options.map((option) => (
-                <option key={option.value} value={option.id}>
+    <div className="add container">
+          <div className="modal1">
+            <span className="close" onClick={() => {props.setOpen(false)}}>
+            X
+          </span>
+            <h1>new {props.name}</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group key="product_name">
+          <Form.Label>product_name</Form.Label>
+          <Form.Control
+            required
+            as="select"
+            name="product_name"
+            value={selectedProduct ? selectedProduct.id : ''}
+            onChange={(e) => handleProductChange(Number(e.target.value))}
+            disabled={false} // You can adjust the disabled attribute as needed
+          >
+            <option value="">None</option>
+            {props.columns[0].options.map((option) => (
+              <option key={option.value} value={option.id}>
+                {option.label}
+              </option>
+            ))}
+          </Form.Control>
+        </Form.Group>
+          
+        <Form.Group key="kind_of_product">
+          <Form.Label>kind_of_product</Form.Label>
+          <Form.Control
+            required
+            as="select"
+            name="kind_of_product"
+            value={selectedKind || ''}
+            onChange={(e) => handleKindChange(e.target.value)}
+            disabled={!selectedProduct} // Disable if no product is selected
+          >
+            <option value="">None</option>
+            {props.columns[1].options
+              .filter((option) => option.id === selectedProduct?.id)
+              .map((option) => (
+                <option key={option.value} value={option.value}>
                   {option.label}
                 </option>
               ))}
-            </Form.Control>
+          </Form.Control>
+        </Form.Group>
+         <Form.Group key="type_of_order">
+          <Form.Label>type_of_order</Form.Label>
+          <Form.Control
+            required
+            as="select"
+            name="type_of_order"
+            value={selectedKind || ''}
+            onChange={(e) => handleKindChange(e.target.value)}
+            disabled={!selectedProduct} // Disable if no product is selected
+          >
+            <option value="">None</option>
+            {props.columns[1].options
+              .filter((option) => option.id === selectedProduct?.id)
+              .map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+          </Form.Control>
           </Form.Group>
-          <Form.Group key="kind_of_product">
-            <Form.Label>kind_of_product</Form.Label>
-            <Form.Control
-              required
-              as="select"
-              name="kind_of_product"
-              value={selectedKind || ''}
-              onChange={(e) => handleKindChange(e.target.value)}
-              disabled={!selectedProduct} // Disable if no product is selected
-            >
-              <option value="">None</option>
-              {props.columns[1].options
-                .filter((option) => option.id === selectedProduct?.id)
-                .map((option) => (
-                  <option key={option.value} value={option.value}>
-                    {option.label}
-                  </option>
-                ))}
-            </Form.Control>
-          </Form.Group>
-          <Form.Group key="total_price">
-            <Form.Label>total_price</Form.Label>
-            <Form.Control
-              type="number"
-              name="total_price"
-              value={totalPrice}
-              disabled
-            />
-          </Form.Group>
-          {/* ... (rest of your JSX) */}
-        </div>
+        <Form.Group key="state_of_order">
+          <Form.Label>state_of_order</Form.Label>
+          <Form.Control
+            required
+            as="select"
+            name="state_of_order"
+            value={selectedKind || ''}
+            onChange={(e) => handleKindChange(e.target.value)}
+            disabled={!selectedProduct} // Disable if no product is selected
+          >
+            <option value="">None</option>
+            {props.columns[1].options
+              .filter((option) => option.id === selectedProduct?.id)
+              .map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+          </Form.Control>
+        </Form.Group>  
+        <Form.Group key="amount">
+          <Form.Label>amount</Form.Label>
+              <Form.Control
+            placeholder='amount'
+            type="number"
+            name="amount"
+            value={amount}
+          />
+        </Form.Group>
+        <Form.Group key="total_price">
+          <Form.Label>total_price</Form.Label>
+          <Form.Control
+            type="number"
+            name="total_price"
+            value={totalPrice}
+            disabled
+          />
+        </Form.Group>
+        <Form.Group key="paid_price">
+          <Form.Label>paid_price</Form.Label>
+          <Form.Control
+            type="number"
+            placeholder='paid_price'
+            name="paid_price"
+            value={paid_price}
+          />
+        </Form.Group>
+        <Form.Group key="remain_price">
+          <Form.Label>remain_price</Form.Label>
+          <Form.Control
+            type="number"
+            placeholder='remain_price'
+            name="remain_price"
+            value={remain_price}
+            disabled='true'
+          />
+        </Form.Group>
+        <Form.Group key="full_name">
+          <Form.Label>full_name</Form.Label>
+          <Form.Control
+                type="text"
+                placeholder='full_name'
+            name="full_name"
+            value={full_name}
+          />
+        </Form.Group>
+        <Form.Group key="phone">
+          <Form.Label>phone</Form.Label>
+          <Form.Control
+                type="text"
+                placeholder='phone'
+            name="phone"
+            value={phone}
+          />
+        </Form.Group>
+        <Form.Group className="item">
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+        </Form.Group>
+      </Form>
       </div>
-    </>
-  );
-};
-
+    </div>
+  </>
+);
+}
 export default Add;
--- src/pages/order/Order.jsx
+++ src/pages/order/Order.jsx
@@ -1,21 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Form, Button } from 'react-bootstrap';
-import AlertExample from '../../components/other/Alert';
-import { server, imageserver } from '../../constants';
+import Datatable from '../../components/dataTable/DataTable';
+import Add from './Add';
+import { server } from '../../constants';
 
-const Add = (props) => {
-   let pro_name = [{}];
+
+const Order = () => {
+  let pro_name = [{}];
   let productsKind = [{}];
   let productsMunit = [{}];
   const [products, setProducts] = useState([]);
-  const { selectedProduct, selectedKind, amount, paid_price, handleProductChange, handleKindChange, full_name } = props;
-  const [form, setForm] = useState({});
-  const [errors, setErrors] = useState({});
-  const [kindOptions, setKindOptions] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [selectedKind, setSelectedKind] = useState(null);
   const [totalPrice, setTotalPrice] = useState(0);
-  const [remainPrice, setRemainPrice] = useState(0);
-
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -30,70 +28,7 @@ const Add = (props) => {
     fetchProducts();
   }, []);
 
-
-  useEffect(() => {
-    const defaultValues = {};
-    props.columns.forEach((column) => {
-      defaultValues[column.field] = column.defaultValue || '';
-    });
-    setForm(defaultValues);
-  }, [props.columns]);
-
-  useEffect(() => {
-    if (selectedProduct) {
-      const filteredKinds = props.columns[1].options.filter(option => option.id === selectedProduct.id);
-      setKindOptions(filteredKinds);
-    }
-  }, [props.columns, selectedProduct]);
-
-  const calculateTotalPrice = () => {
-    if (selectedProduct && selectedKind) {
-      const priceColumn = selectedKind === 'home_made' ? 'home_price' : 'out_price';
-      const selectedProductData = props.columns[0].options.find(option => option.id === selectedProduct.id);
-      return amount * selectedProductData[priceColumn];
-    }
-    return 0;
-  };
-
-  useEffect(() => {
-    const calculatedTotalPrice = calculateTotalPrice();
-    setTotalPrice(calculatedTotalPrice);
-    setRemainPrice(calculatedTotalPrice - paid_price);
-  }, [amount, selectedProduct, selectedKind, paid_price]);
-
-  const setField = (field, value) => {
-    setForm(prevForm => ({
-      ...prevForm,
-      [field]: value,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (validateForm()) {
-      const formData = new FormData();
-      Object.entries(form).forEach(([key, value]) => {
-        formData.append(key, value);
-      });
-      axios
-        .post(`${imageserver}addNew${props.name}/`, formData)
-        .then((response) => {
-          props.setOpen(false);
-          // Show success message or handle response as needed
-          console.log(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-  };
-
-  const validateForm = () => {
-    // Implement your form validation logic
-    return true; // Placeholder, replace with your validation
-  };
-  
-   if (products) { 
+  if (products) { 
     products.map((single,i) => { 
       pro_name.push(
          { label: single.product_name, value: single.product_name,id:i  }
@@ -103,9 +38,29 @@ const Add = (props) => {
       )
     })
   }
+  const handleProductChange = (selectedProductId) => {
+    const selectedProduct = products.find((product) => product.id === selectedProductId);
+    setSelectedProduct(selectedProduct);
+    setSelectedKind(null);
+    setTotalPrice(0);
+  };
 
+  const handleKindChange = (selectedKind) => {
+    setSelectedKind(selectedKind);
+    if (selectedProduct && selectedKind) {
+      // Fetch the price from the server based on selectedProduct and selectedKind
+      const calculatedTotalPrice = calculateTotalPrice(selectedProduct, selectedKind);
+      setTotalPrice(calculatedTotalPrice);
+    }
+  };
 
-  const columns = [
+  const calculateTotalPrice = (product, kind) => {
+    
+    // Implement your logic to fetch and calculate the total price
+    return 0; // Placeholder value, replace with your actual calculation
+  };
+
+   const columns = [
   {
     field: 'product_name',
     headerName: 'product_name',
@@ -188,76 +143,28 @@ const Add = (props) => {
 ];
 
   return (
-    <div className="add container">
-      <div className="modal1">
-        <span className="close" onClick={() => {props.setOpen(false)}}>
-          X
-        </span>
-        <h1>new {props.name}</h1>
-        <Form onSubmit={handleSubmit}>
-          {/* Rest of your form inputs */}
-          {/* Example: */}
-          <Form.Group key="product_name">
-            <Form.Label>product_name</Form.Label>
-            <Form.Control
-              required
-              as="select"
-              name="product_name"
-              value={selectedProduct ? selectedProduct.id : ''}
-              onChange={(e) => handleProductChange(Number(e.target.value))}
-              disabled={false}
-            >
-              {/* Your options here */}
-            </Form.Control>
-          </Form.Group>
-
-          {/* ... (other form fields) */}
-          
-          <Form.Group key="type_of_order">
-            <Form.Label>type_of_order</Form.Label>
-            <Form.Control
-              required
-              as="select"
-              name="type_of_order"
-              value={selectedKind || ''}
-              onChange={(e) => handleKindChange(e.target.value)}
-              disabled={!selectedProduct}
-            >
-              {/* Your options here */}
-            </Form.Control>
-          </Form.Group>
-
-          {/* ... (rest of your form) */}
-
-          <Form.Group key="total_price">
-            <Form.Label>total_price</Form.Label>
-            <Form.Control
-              type="number"
-              name="total_price"
-              value={totalPrice}
-              disabled
-            />
-          </Form.Group>
-          <Form.Group key="remain_price">
-            <Form.Label>remain_price</Form.Label>
-            <Form.Control
-              type="number"
-              name="remain_price"
-              value={remainPrice}
-              disabled
-            />
-          </Form.Group>
-
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+    <div className="products container">
+      <div className="info center">
+        <h1>Orders</h1>
+        <button className="btn btn-primary" onClick={() => setOpen(true)}>
+          Add new order
+        </button>
       </div>
+      <Datatable first="orders" />
+      {open && (
+        <Add
+          name="orders"
+          columns={columns}
+          setOpen={setOpen}
+          selectedProduct={selectedProduct}
+          selectedKind={selectedKind}
+          totalPrice={totalPrice}
+          handleProductChange={handleProductChange}
+          handleKindChange={handleKindChange}
+        />
+      )}
     </div>
   );
-}
-
-export default Add;
+};
 
 export default Order;
-
