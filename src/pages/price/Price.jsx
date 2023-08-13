622d0ed4371dbe7709c441fc19c42a0fed694796
zeru0429 <mihiretutigistu@gmail.com>
2023-08-13 13:39:22 +0300

    add some stylings on price page

--- src/pages/price/Price.jsx
+++ src/pages/price/Price.jsx
@@ -1,7 +1,9 @@
 import React ,{useState,useEffect}from 'react'
 import axios from 'axios'
+import './price.css'
 import BorderExample from '../../components/card/BorderExample'
 import BasicExample from '../../components/card/BasicExample'
+import { Container, Row, Col } from 'react-bootstrap';
 import { imageserver,server } from '../../constants'
 const Price = () => {
   const[product,setProduct]= useState([])
@@ -26,18 +28,22 @@ const Price = () => {
 
   
     //console.log(product);
-  return (  
-    <div className='container d-flex m-5 col-12'>
-      {product.map((item) => {
-        return (
-          <div key={item.id} className='row'>
-            <div className='col-4'>
-            <BasicExample image_url={`${imageserver}${item.image_url}`} title={item.product_name} description={item.date_of_update}/>
+ return (
+    <Container className='d-flex justify-content-center align-items-center ms-0 mr-0 mt-auto mb-auto '>
+      <Row>
+        {product.map((item) => (
+          <Col key={item.id} md={4} className='mb-4'>
+            <div className='product-card'>
+              <BasicExample
+                image_url={`${imageserver}${item.image_url}`}
+                title={item.product_name}
+                description={item.date_of_update}
+              />
             </div>
-          </div>
-        );
-      })}
-    </div> 
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
   
   
